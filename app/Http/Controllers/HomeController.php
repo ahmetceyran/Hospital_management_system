91b248fb707385f96db8cd4f3693956a6ae7193a
ahmetceyran <ahmetceyran00@gmail.com>
2023-11-24 19:46:56 +0300

    Add display doctor data in userpage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Doctor;
 
 class HomeController extends Controller
 {
@@ -23,8 +24,9 @@ class HomeController extends Controller
 
             else
             {
+                $doctor = doctor::all();
 
-                return view('user.home');
+                return view('user.home', compact('doctor'));
 
             }
 
@@ -41,7 +43,18 @@ class HomeController extends Controller
 
     public function index()
     {
-        return view('user.home');
+        if(Auth::id())
+        {
+            return redirect('home');
+        }
+
+        else
+        {
+            $doctor = doctor::all();
+
+            return view('user.home', compact('doctor'));
+        }
+
     }
 
 }
